0dfae752e8df0432c235305d5ef36e774054bb66
mirogaudi <mirogaudi@ya.ru>
2022-01-29 00:41:30 +0100

    Suppress warning

--- src/main/kotlin/mirogaudi/memo/MemoServiceApplication.kt
+++ src/main/kotlin/mirogaudi/memo/MemoServiceApplication.kt
@@ -7,5 +7,6 @@ import org.springframework.boot.runApplication
 class MemoServiceApplication
 
 fun main(args: Array<String>) {
+    @Suppress("SpreadOperator")
     runApplication<MemoServiceApplication>(*args)
 }
