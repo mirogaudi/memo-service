e74778605d22031388ccdee8fdefcbfef84f1f38
mirogaudi <mirogaudi@ya.ru>
2022-04-26 23:05:12 +0200

    Add Services

---
+++ src/main/kotlin/mirogaudi/memo/service/LabelService.kt
@@ -0,0 +1,63 @@
+package mirogaudi.memo.service
+
+import mirogaudi.memo.domain.Label
+import mirogaudi.memo.repository.LabelRepository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+interface LabelService {
+    fun getAll(): List<Label>
+    fun getById(id: Long): Label
+    fun deleteById(id: Long)
+    fun create(label: Label): Label
+    fun update(id: Long, label: Label): Label
+}
+
+@Service
+@Transactional
+class LabelServiceImpl(val labelRepository: LabelRepository) : LabelService {
+
+    override fun getAll(): List<Label> {
+        return labelRepository.findAll()
+    }
+
+    override fun getById(id: Long): Label {
+        return labelRepository.findById(id)
+            .orElseThrow { labelNotFoundException(id) }
+    }
+
+    override fun deleteById(id: Long) {
+        when {
+            labelRepository.existsById(id) -> {
+                labelRepository.deleteById(id)
+            }
+            else -> throw labelNotFoundException(id)
+        }
+    }
+
+    override fun create(label: Label): Label {
+        return labelRepository.save(label)
+    }
+
+    override fun update(id: Long, label: Label): Label {
+        return when {
+            labelRepository.existsById(id) -> {
+                labelRepository.save(
+                    Label(
+                        id = label.id,
+                        name = label.name
+                    )
+                )
+            }
+            else -> {
+                throw labelNotFoundException(id)
+            }
+        }
+    }
+
+    private fun labelNotFoundException(id: Long) = NotFoundException("Label with id='$id' not found")
+
+    // TODO add tests
+}
+
+
---
+++ src/main/kotlin/mirogaudi/memo/service/MemoService.kt
@@ -0,0 +1,64 @@
+package mirogaudi.memo.service
+
+import mirogaudi.memo.domain.Memo
+import mirogaudi.memo.repository.MemoRepository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+interface MemoService {
+    fun getAll(): List<Memo>
+    fun getById(id: Long): Memo
+    fun deleteById(id: Long)
+    fun create(memo: Memo): Memo
+    fun update(id: Long, memo: Memo): Memo
+}
+
+@Service
+@Transactional
+class MemoServiceImpl(val memoRepository: MemoRepository) : MemoService {
+
+    override fun getAll(): List<Memo> {
+        return memoRepository.findAll()
+    }
+
+    override fun getById(id: Long): Memo {
+        return memoRepository.findById(id)
+            .orElseThrow { memoNotFoundException(id) }
+    }
+
+    override fun deleteById(id: Long) {
+        when {
+            memoRepository.existsById(id) -> {
+                memoRepository.deleteById(id)
+            }
+            else -> throw memoNotFoundException(id)
+        }
+    }
+
+    override fun create(memo: Memo): Memo {
+        return memoRepository.save(memo)
+    }
+
+    override fun update(id: Long, memo: Memo): Memo {
+        return when {
+            memoRepository.existsById(id) -> {
+                memoRepository.save(
+                    Memo(
+                        id = memo.id,
+                        text = memo.text,
+                        labels = memo.labels,
+                        dueDate = memo.dueDate
+                    )
+                )
+            }
+            else -> {
+                throw memoNotFoundException(id)
+            }
+        }
+    }
+
+    private fun memoNotFoundException(id: Long) = NotFoundException("Memo with id='$id' not found")
+
+    // TODO add fun getByLabel
+    // TODO add tests
+}
