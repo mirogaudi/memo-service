133ba3be2278784b3d65022903b8b9882cff5357
mirogaudi <mirogaudi@ya.ru>
2022-04-26 22:44:54 +0200

    Update domain classes

--- src/main/kotlin/mirogaudi/memo/domain/Label.kt
+++ src/main/kotlin/mirogaudi/memo/domain/Label.kt
@@ -1,24 +1,42 @@
 package mirogaudi.memo.domain
 
+import org.hibernate.Hibernate
 import javax.persistence.Column
 import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
 import javax.persistence.Table
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
-/**
- * Memo label entity.
- */
 @Entity
 @Table(name = "label")
-class Label : BaseEntity() {
+data class Label(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    var id: Long? = null,
 
     @Column(name = "name", nullable = false, unique = true)
     @NotNull
     @Size(min = 3, max = 128)
     var name: String? = null
 
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
+        other as Label
+
+        return id != null && id == other.id
+    }
+
+    override fun hashCode(): Int = javaClass.hashCode()
+
+    @Override
     override fun toString(): String {
-        return "Label(id=$id, name=$name)"
+        return this::class.simpleName + "(id = $id , name = $name )"
     }
 }
--- src/main/kotlin/mirogaudi/memo/domain/Memo.kt
+++ src/main/kotlin/mirogaudi/memo/domain/Memo.kt
@@ -1,11 +1,17 @@
 package mirogaudi.memo.domain
 
+import com.fasterxml.jackson.annotation.JsonGetter
+import com.fasterxml.jackson.annotation.JsonIgnore
+import org.hibernate.Hibernate
 import org.springframework.data.annotation.CreatedDate
 import java.time.LocalDateTime
 import javax.persistence.CascadeType
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.FetchType
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
 import javax.persistence.JoinColumn
 import javax.persistence.JoinTable
 import javax.persistence.ManyToMany
@@ -13,17 +19,19 @@ import javax.persistence.Table
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
-/**
- * Simple memo entity.
- */
 @Entity
 @Table(name = "memo")
-class Memo : BaseEntity() {
+data class Memo(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    var id: Long? = null,
 
     @Column(name = "text", nullable = false)
     @NotNull
     @Size(min = 3, max = 1024)
-    var text: String? = null
+    var text: String? = null,
 
     @ManyToMany(fetch = FetchType.EAGER, cascade = [CascadeType.ALL])
     @JoinTable(
@@ -31,12 +39,34 @@ class Memo : BaseEntity() {
         joinColumns = [JoinColumn(name = "memo_id", referencedColumnName = "id")],
         inverseJoinColumns = [JoinColumn(name = "label_id", referencedColumnName = "id")]
     )
-    var labels: MutableSet<Label> = mutableSetOf()
+    @JsonIgnore
+    var labels: MutableSet<Label> = mutableSetOf(),
 
     @CreatedDate
     @Column(nullable = false)
-    var createdDate: LocalDateTime? = null
+    var createdDate: LocalDateTime? = null,
 
     @Column(nullable = true)
     var dueDate: LocalDateTime? = null
+
+) {
+    @JsonGetter
+    fun labelNames(): List<String> {
+        return labels.map { it.name.toString() }.sorted()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
+        other as Memo
+
+        return id != null && id == other.id
+    }
+
+    override fun hashCode(): Int = javaClass.hashCode()
+
+    @Override
+    override fun toString(): String {
+        return this::class.simpleName + "(id = $id , text = $text , createdDate = $createdDate , dueDate = $dueDate )"
+    }
 }
