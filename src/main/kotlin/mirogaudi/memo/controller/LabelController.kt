2433d0372d6a5db9531f13a99f2058f3499b3d8f
mirogaudi <mirogaudi@ya.ru>
2022-04-27 14:08:49 +0200

    Extend code

--- src/main/kotlin/mirogaudi/memo/controller/LabelController.kt
+++ src/main/kotlin/mirogaudi/memo/controller/LabelController.kt
@@ -8,8 +8,12 @@ import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/api/v1/labels")
@@ -24,20 +28,32 @@ class LabelController(val labelService: LabelService) {
 
     @GetMapping(value = ["/{id}"])
     fun getById(
-        @Parameter(description = "Label ID")
-        @PathVariable id: Long
+        @Parameter(description = "Label ID") @PathVariable id: Long
     ): Label {
         return labelService.getById(id)
     }
 
     @DeleteMapping(value = ["/{id}"])
     fun deleteById(
-        @Parameter(description = "Label ID")
-        @PathVariable id: Long
+        @Parameter(description = "Label ID") @PathVariable id: Long
     ) {
         labelService.deleteById(id)
     }
 
-    // TODO add missing code
+    @PostMapping
+    fun create(
+        @Parameter(description = "Label") @Valid @RequestBody label: Label
+    ): Label {
+        return labelService.create(label)
+    }
+
+    @PutMapping(value = ["/{id}"])
+    fun update(
+        @Parameter(description = "Label ID") @PathVariable id: Long,
+        @Parameter(description = "Label") @Valid @RequestBody label: Label
+    ): Label {
+        return labelService.update(id, label)
+    }
+
     // TODO add tests
 }
