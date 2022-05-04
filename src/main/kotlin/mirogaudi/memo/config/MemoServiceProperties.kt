ca7a8bbb319ef3b4bbd2f71605d23e89e3a189e7
mirogaudi <mirogaudi@ya.ru>
2022-05-04 12:36:02 +0200

    Add configuration properties

---
+++ src/main/kotlin/mirogaudi/memo/config/MemoServiceProperties.kt
@@ -0,0 +1,9 @@
+package mirogaudi.memo.config
+
+import mirogaudi.memo.domain.Priority
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties(prefix = "ms")
+class MemoServiceProperties {
+    var priority: Priority? = null
+}
