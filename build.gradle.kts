c27a56c4b09191a4c7e6d6a408953f11987a60ad
mirogaudi <mirogaudi@ya.ru>
2022-05-30 22:01:39 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,7 +60,7 @@ dependencies {
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
-    val springdocVersion = "1.6.8"
+    val springdocVersion = "1.6.9"
     implementation("org.springdoc:springdoc-openapi-ui:$springdocVersion")
     implementation("org.springdoc:springdoc-openapi-kotlin:$springdocVersion")
 
