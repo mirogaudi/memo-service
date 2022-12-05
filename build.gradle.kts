1a00fd1f95d47e9ed4e1a5febdfb462a4aee1642
mirogaudi <mirogaudi@ya.ru>
2022-12-05 22:13:46 +0100

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -69,7 +69,7 @@ dependencies {
     testImplementation(kotlin("test"))
 
     testImplementation("io.mockk:mockk:1.13.3")
-    testImplementation("com.ninja-squad:springmockk:3.1.2")
+    testImplementation("com.ninja-squad:springmockk:4.0.0")
 }
 
 springBoot {
