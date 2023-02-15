274982ba5e47ecae355fe30df32557e7d6083eab
mirogaudi <mirogaudi@ya.ru>
2023-02-15 10:43:08 +0100

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ plugins {
     jacoco
     id("org.jetbrains.kotlinx.kover") version "0.6.1"
 
-    id("org.owasp.dependencycheck") version "8.0.2"
+    id("org.owasp.dependencycheck") version "8.1.0"
     id("com.github.ben-manes.versions") version "0.45.0"
 
     id("org.springdoc.openapi-gradle-plugin") version "1.6.0"
